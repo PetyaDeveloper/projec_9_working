a9f84d13bb52389aaf15fa517b6336767cbe6f1f
PETYA STEFANOVA <PSKPETYA@gmx.com>
2018-11-01 21:44:02 +0400

    meet expectations done

--- js/app.js
+++ js/app.js
@@ -46,11 +46,12 @@ var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
         type: 'line',
         data: {
-            //label: 'none',
+            
             labels:["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
             //fontSize: 20,
             datasets: [{
-            data: [0, 5, 3, 8, 9, 3,0,9,6,12,18],
+            data: [0, 500, 1000, 750, 1750, 1250,1500,1000,1500,2000,1500,2000],
+            
             backgroundColor: '#e6e6ff',
            borderColor: '#751aff',
            borderWidth: 1
@@ -63,8 +64,12 @@ var myChart = new Chart(ctx, {
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero:true
-                }
+                    beginAtZero:false,
+                    suggestedMin: 550,
+                    suggestedMax: 2500,
+                    stepSize: 500
+                },
+                
             }]
         }
     },
@@ -78,10 +83,12 @@ var myChartBar = new Chart(ctx, {
         type: 'bar',
         data: {
             //label: 'none',
+            
             labels:["S", "M", "T", "W", "T", "F", "S"],
             //fontSize: 20,
             datasets: [{
-            data: [0, 5, 3, 8, 9, 3,0,9,6,12,18],
+                
+            data: [50, 80, 150, 100, 200, 180,75],
             backgroundColor: '#7979d2',
            borderColor: '#751aff',
            borderWidth: 1
@@ -91,46 +98,74 @@ var myChartBar = new Chart(ctx, {
       legend: {
            display: false
         },
-        scales:{yAxes: [{
-                ticks: {beginAtZero:true}
+        scales:{
+        yAxes: [{ 
+      gridLines: {display: true},
+  
+            
+                ticks: {beginAtZero:true,
+                       suggestedMin: 50,
+                    suggestedMax: 250,
+                    stepSize: 50
+                       }
                }]
-    }
-    },
-    responsive:true
+        },
+      },
+    responsive:true,
+    barThickness: 1
 });
 // CHART 3 DONUT
 var ctx = document.getElementById("myChartDonut");
 var myChartDonut = new Chart(ctx, {
         type: 'doughnut',
         data: {
-               labels: [
-        'Red',
-        'Yellow',
-        'Blue'
-    ],
-            //label: 'mobile',
-            //labels:["16-22", "23-299", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
-            //fontSize: 20,
-            datasets: [{
-            data: [0, 5, 3, 8, 9, 3,0,9,6,12,18],
-            backgroundColor: '#e6e6ff',
+               labels: [ 'Phones', 'Tablets', 'Desktop'],
+              
+           datasets: [{
+           data: [20,15,65],
+           backgroundColor: ['#809fff','#66cc66','#666699'],
            borderColor: '#751aff',
            borderWidth: 1
            }]
         },
         options: {
-          legend: {
-               display: true,
+            layout: {
+            padding: {
+                left: 0,
+                right: 70,
+                top: 30,
+                bottom: 0
             }
+        },
+          legend: {
+//            display: true,
+              position: 'right',
+              labels: {
+                  boxWidth: 15,
+                  padding: 20
+              },
+          },
+            
         },
        responsive:true
 });
 ctx.style.height = '250px';
 
 
+//Create a “Send” button and use JS to allow you to submit the form and display a confirmation the message was sent. You //won't actually submit the form, just simulate the action using JavaScript.
+//Use JS to display error messages if user isn’t selected or message field is empty.
+const sendBtn = document.querySelector('.submitBtn');
+const input = document.querySelector("#searchForUser");
+const textArea = document.getElementsByClassName('textArea')[0];
 
-
-
+sendBtn.addEventListener('click',()=>{
+       if(textArea.value == "" || input.value == "") {
+      alert("Error: Please, write in both fields: search for user and send a message, in order to submit the form.");
+      //textArea.focus();
+    }else {
+        alert('YOUR FORM HAS BEEN SUCCESSFULLY SUBMITTED');
+    }
+    });
 
 
 
